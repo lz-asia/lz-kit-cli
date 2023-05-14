6a84545569d036273edb8e287d61c0aa769a56fa
LevX <team@levx.io>
2023-05-15 06:54:07 +0900

    fix: remove contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,1 @@
-module.exports = {
-    solidity: {
-        version: "0.8.17",
-        settings: {
-            optimizer: {
-                enabled: true,
-                runs: 200,
-            },
-            viaIR: true,
-        },
-    },
-};
+module.exports = {};
