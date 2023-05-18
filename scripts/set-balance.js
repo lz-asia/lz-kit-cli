ccdb0c44108c296defb0ece718a32d59e6099f35
LevX <team@levx.io>
2023-05-18 15:01:38 +0900

    fix: script set-balance.js doesn't accept number of accounts

--- scripts/set-balance.js
+++ scripts/set-balance.js
@@ -11,9 +11,8 @@ async function main() {
         throw new Error("Cannot find network " + process.env.NETWORK);
     }
     const provider = new providers.JsonRpcProvider(network.url, network.chainId);
-    const count = process.env.COUNT || 1;
-    for (let i = 0; i < count; i++) {
-        const wallet = deriveWallet(provider, process.env.MNEMONIC);
+    for (let i = 0; i < (Number(process.env.ACCOUNTS) || 1); i++) {
+        const wallet = deriveWallet(provider, process.env.MNEMONIC, i);
         const address = await wallet.getAddress();
         await provider.send("hardhat_setBalance", [address, utils.hexValue(utils.parseEther(process.env.BALANCE))]);
         console.log("Set balance of " + address + " to " + process.env.BALANCE);
